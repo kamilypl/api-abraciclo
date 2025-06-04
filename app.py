cdbbf0245f44c3b5ef0de1e4a5e2f7936d6b14c2
Kamilly Prado Lopes <kpl.eng21@uea.edu.br>
2025-06-04 10:40:23 -0400

    DEPLOY

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
 from flask import Flask, request, jsonify
 import pdfplumber
 import pandas as pd
-import os
+from io import BytesIO
 
 app = Flask(__name__)
 
 @app.route('/extrair-tabelas', methods=['POST'])
 def extrair_tabelas():
-    # Diretórios
-    pasta_entrada = "entrada_pdfs"
-    pasta_saida = "saida_excel"
-    # Nome fixo para o PDF de entrada
-    nome_entrada = "entrada.pdf"
-    caminho_entrada = os.path.join(pasta_entrada, nome_entrada)
-    caminho_saida = os.path.join(pasta_saida, "saida.xlsx")
-    
-    # Cria as pastas se não existirem
-    os.makedirs(pasta_entrada, exist_ok=True)
-    os.makedirs(pasta_saida, exist_ok=True)
-
     file = request.files['file']
-
-    # Remove o arquivo anterior se existir
-    if os.path.exists(caminho_entrada):
-        os.remove(caminho_entrada)
-
-    # Salva sempre como "entrada.pdf"
-    file.save(caminho_entrada)
-    
+    # O segredo aqui: trabalhar com BytesIO
     CABECALHO_PADRAO = ["MODELOS", "cm³", "JAN", "FEV", "MAR", "ABR", "TOTAL", "%"]
     todas_as_tabelas = []
-    with pdfplumber.open(caminho_entrada) as pdf:
+    with pdfplumber.open(BytesIO(file.read())) as pdf:
         for pagina in pdf.pages:
             tabelas = pagina.extract_tables()
             for tabela in tabelas:
@@ -39,8 +20,18 @@ def extrair_tabelas():
                     todas_as_tabelas.append(df)
     if todas_as_tabelas:
         df_final = pd.concat(todas_as_tabelas, ignore_index=True)
-        df_final.to_excel(caminho_saida, index=False)
-        return jsonify({"status": "sucesso", "excel": caminho_saida})
+        # Agora vamos devolver o Excel direto como resposta, sem salvar no disco!
+        output = BytesIO()
+        df_final.to_excel(output, index=False)
+        output.seek(0)
+        return (
+            output.getvalue(),  # Binário do arquivo
+            200,
+            {
+                "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "Content-Disposition": "attachment; filename=saida.xlsx"
+            }
+        )
     else:
         return jsonify({"status": "erro", "msg": "Nenhuma tabela encontrada"}), 400
 
